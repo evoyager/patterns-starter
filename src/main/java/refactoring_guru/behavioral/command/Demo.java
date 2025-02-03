94e7379ec7fedab90b8a99cbc6134d5f57e6a523
Ievgen Gusar <evgeniygusar@gmail.com>
2025-02-04 01:31:37 +0200

    Behavioral: Command

---
+++ src/main/java/refactoring_guru/behavioral/command/Demo.java
@@ -0,0 +1,10 @@
+package refactoring_guru.behavioral.command;
+
+import refactoring_guru.behavioral.command.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
